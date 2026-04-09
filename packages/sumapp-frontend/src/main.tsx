2395d5370a82257dabfef240c2368dd7ea8f57c1
caroWD <juego.fuego@gmail.com>
2026-04-09 12:55:57 -0500

    feat(packages/sumapp-frontend): implement theme provider

--- packages/sumapp-frontend/src/main.tsx
+++ packages/sumapp-frontend/src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import router from '@components/routers/router'
+import ThemeProvider from '@components/providers/ThemeProvider'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 )
